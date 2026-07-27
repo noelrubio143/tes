aa4dd0904998fc49f289973d1ea8517934bad038
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-27 22:46:56 +0800

    Replace 'Clear RAM Cache' option with 'Reboot VPS'

--- menu.sh
+++ menu.sh
@@ -64,8 +64,7 @@ show_menu() {
     echo -e "\e[1;36m 2 \e[0m: Menu Vless"
     echo -e "\e[1;36m 3 \e[0m: Menu Setting"
     echo -e "\e[1;36m 4 \e[0m: Status Service"
-    echo -e "\e[1;36m 5 \e[0m: Clear RAM Cache"
-    echo -e "\e[1;36m 6 \e[0m: Reboot VPS"
+    echo -e "\e[1;36m 5 \e[0m: Reboot VPS"
     echo -e "\e[1;36m x \e[0m: Exit Script"
     echo -e ""
     echo -e "\e[1;33m -------------------------------------------------\e[0m"
@@ -82,8 +81,7 @@ show_menu() {
     2) clear ; m-vless ;;
     3) clear ; m-system ;;
     4) clear ; running ;;
-    5) clear ; clearcache ;;
-    6) clear ; /sbin/reboot ;;
+    5) clear ; /sbin/reboot ;;
     x) exit ;;
     *) echo "Invalid selection" ; sleep 1 ;;
     esac
