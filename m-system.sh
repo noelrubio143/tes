62a2b712addb09b43bec021d151e0975d6e60b24
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-27 22:29:54 +0800

    Rename system menu from SENSI to AMBERVPN

--- m-system.sh
+++ m-system.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • SENSI SYSTEM MENU •          \E[0m"
+echo -e "\E[0;100;33m          • AMBERVPN SYSTEM MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Panel Domain"
