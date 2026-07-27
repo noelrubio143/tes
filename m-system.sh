ed01174f2160eafb2c3e67f754b07fa71231632a
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-27 22:29:29 +0800

    Remove option to restart all services from menu

--- m-system.sh
+++ m-system.sh
@@ -7,7 +7,6 @@ echo -e "\E[0;100;33m          • SENSI SYSTEM MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Panel Domain"
-echo -e " [\e[36m•2\e[0m] Restart All Service"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO SENSI MENU\033[0m"
 echo -e   ""
@@ -19,7 +18,6 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; m-domain ; exit ;;
-2) clear ; restart ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "worng options" ; sleep 1 ; m-system ;;
